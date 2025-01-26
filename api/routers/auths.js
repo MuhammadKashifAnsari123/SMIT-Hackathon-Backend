11510409f29441e1f1a732d70b49f229b020c19b
MuhammadKashifAnsari123 <muhammadkashifa82@gmail.com>
2025-01-26 08:38:40 +0500

    backend

--- api/routers/auths.js
+++ api/routers/auths.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import Users from "../models/Users.js";
+import Users from "../api/models/Users.js";
 const router = express.Router();
 import "dotenv/config";
 
